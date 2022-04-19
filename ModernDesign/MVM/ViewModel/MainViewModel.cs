c947065cf1339ecc8187d8481f46d6bab524b40b
Victor <victor.alexanderzhao@gmail.com>
2022-04-19 17:53:17 -0300

    Projeto Morder Flat UI

---
+++ ModernDesign/MVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
